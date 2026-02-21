281cc132d4fe3166a906475e3f274ea87d4811bf
Diego Frias <mail@dzfrias.dev>
2026-02-20 20:39:02 -0600

    Remove unused directory

--- static/js/script.js
+++
@@ -1,9 +0,0 @@
-const transactionsElem = document.getElementById("transactions-body")
-
-async function updateTransactions(tbody) {
-    const response = await fetch("/api/transactions")
-    const transactions = await response.json()
-    console.log(transactions)
-}
-
-updateTransactions(transactionsElem)
